3162c4dd9a205e8893c561b6de873d3034111609
Dave-log <david.garcia45@hotmail.fr>
2025-01-24 23:20:41 +0100

    zzdazd

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-import { OBSWebSocket } from 'obs-websocket-js';
+import OBSWebSocket from 'https://cdn.jsdelivr.net/npm/obs-websocket-js/dist/obs-websocket.min.js';
 
 const obs = new OBSWebSocket();
 
