0083c7770c3d2a52761a871165526b12dbad7a70
Dave-log <david.garcia45@hotmail.fr>
2025-01-26 10:57:05 +0100

    test!

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-const OBSWebSocket = window.OBSWebSocket; // Import depuis le CDN
 const obs = new OBSWebSocket();
 
 (async () => {
     try {
         // Connexion à OBS
-        await obs.connect("ws://localhost:4455", "ton_mot_de_passe"); // Si pas de mot de passe, omets-le
+        await obs.connect("ws://localhost:4455"); // Si pas de mot de passe, omets-le
         console.log("Connecté à OBS WebSocket !");
     } catch (error) {
         console.error("Erreur de connexion :", error);
