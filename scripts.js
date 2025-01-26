351de705d1cfbe4819cde7d38a16dbf5c045d82b
Dave-log <david.garcia45@hotmail.fr>
2025-01-26 10:52:08 +0100

    change address to url

--- scripts.js
+++ scripts.js
@@ -20,7 +20,7 @@ const merenFrame = document.getElementById("meren-frame");
 const gardokPortrait = document.getElementById("gardok-portrait");
 const gardokFrame = document.getElementById("gardok-frame");
 
-obs.connect({ address: "ws://127.0.0.1:4455"})
+obs.connect({ url: "ws://127.0.0.1:4455"})
     .then(() => {
         console.log("Connecté à OBS WebSocket !");
 
