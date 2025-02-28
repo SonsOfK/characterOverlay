74c58eb7f5ebcf39a38a3fd4c9a68646dc106b6e
Dave-log <david.garcia45@hotmail.fr>
2025-02-28 21:04:22 +0100

    opacity volume

--- scripts.js
+++ scripts.js
@@ -26,6 +26,11 @@ let timers =  {
 }
 const delayBeforeHiding = 1000; // 1s
 
+merenPortrait.src = assets.meren.portraitOn;
+merenFrame.src = assets.meren.frameOn;
+gardokPortrait.src = assets.gardok.portraitOn;
+gardokFrame.src = assets.gardok.frameOn;
+
 (async () => {
     try {
         // Connexion à OBS
@@ -64,13 +69,22 @@ function handleAudioLevel(levelDb, portraitElement, frameElement, asset, timer) 
             timer = null;
         }
 
-        portraitElement.src = asset.portraitOn;
-        frameElement.src = asset.frameOn;
+        portraitElement.classList.add("active");
+        portraitElement.classList.remove("inactive");
+        frameElement.classList.add("active");
+        frameElement.classList.remove("inactive");
+
+        //portraitElement.src = asset.portraitOn;
+        //frameElement.src = asset.frameOn;
     } else {
         if (!timer) {
             timer = setTimeout(() => {
-                portraitElement.src = asset.portraitOff;
-                frameElement.src = asset.frameOff;
+                //portraitElement.src = asset.portraitOff;
+                //frameElement.src = asset.frameOff;
+                portraitElement.classList.add("inactive");
+                portraitElement.classList.remove("active");
+                frameElement.classList.add("inactive");
+                frameElement.classList.remove("active");
                 timer = null;
             }, delayBeforeHiding);
         }
