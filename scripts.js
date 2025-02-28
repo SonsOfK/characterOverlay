dc7e195f39b4bc7042b7b335a62424d1459c96ae
Dave-log <david.garcia45@hotmail.fr>
2025-02-28 21:13:40 +0100

    change opacity images

--- scripts.js
+++ scripts.js
@@ -26,11 +26,6 @@ let timers =  {
 }
 const delayBeforeHiding = 1000; // 1s
 
-merenPortrait.src = assets.meren.portraitOn;
-merenFrame.src = assets.meren.frameOn;
-gardokPortrait.src = assets.gardok.portraitOn;
-gardokFrame.src = assets.gardok.frameOn;
-
 (async () => {
     try {
         // Connexion à OBS
@@ -69,22 +64,13 @@ function handleAudioLevel(levelDb, portraitElement, frameElement, asset, timer) 
             timer = null;
         }
 
-        portraitElement.classList.add("active");
-        portraitElement.classList.remove("inactive");
-        frameElement.classList.add("active");
-        frameElement.classList.remove("inactive");
-
-        //portraitElement.src = asset.portraitOn;
-        //frameElement.src = asset.frameOn;
+        portraitElement.src = asset.portraitOn;
+        frameElement.src = asset.frameOn;
     } else {
         if (!timer) {
             timer = setTimeout(() => {
-                //portraitElement.src = asset.portraitOff;
-                //frameElement.src = asset.frameOff;
-                portraitElement.classList.add("inactive");
-                portraitElement.classList.remove("active");
-                frameElement.classList.add("inactive");
-                frameElement.classList.remove("active");
+                portraitElement.src = asset.portraitOff;
+                frameElement.src = asset.frameOff;
                 timer = null;
             }, delayBeforeHiding);
         }
