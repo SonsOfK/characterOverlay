dacd37bbcf7a4d4d0c955d77df9b93ba8195cd2d
Dave-log <david.garcia45@hotmail.fr>
2025-08-01 20:42:11 +0200

    address

--- overlay-client.js
+++ overlay-client.js
@@ -1,7 +1,7 @@
 let ws;
 
 function connectWebSocket() {
-    ws = new WebSocket("wss://af8026fbc522.ngrok-free.app");
+    ws = new WebSocket("wss://4769e70cbd98.ngrok-free.app");
 
     ws.onopen = () => {
         console.log("✅ Connected to WebSocket server");
